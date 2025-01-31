3c37c1e4ef9969a5edceaba252fe5c00de7103e2
HeBa-15 <hebamahmoudali1510@gmail.com>
2025-01-31 18:34:20 +0200

    LINQ - Restriction Operators

---
+++ Assigment/Program.cs
@@ -0,0 +1,70 @@
+﻿using static Assigment.ListGenerators;
+
+
+namespace Assigment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region LINQ - Restriction Operators
+
+
+            #region Q 1. Find all products that are out of stock.
+
+            // var result = ProductList.Where(P => P.UnitsInStock == 0);
+
+            //var result = from p in ProductList
+            //             where p.UnitsInStock == 0
+            //             select p;
+
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            #endregion
+
+            #region Q 2. Find all products that are in stock and cost more than 3.00 per unit.
+
+            //var result = ProductList.Where(p => p.UnitsInStock > 0 && p.UnitPrice > 3.00m);
+
+            //var result = from p in ProductList
+            //             where p.UnitsInStock > 0
+            //             && p.UnitPrice > 3.00m
+            //             select p;
+
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            #endregion
+
+
+            #region Q 3. Returns digits whose name is shorter than their value.
+
+
+
+           // String[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+            //var result = Arr
+            //.Select((name, index) => new { Name = name, Value = index })
+            //.Where(x => x.Name.Length < x.Value)
+            //.Select(x => x.Name);
+
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+
+            #endregion
+
+            #endregion
+        }
+    }
+}
--- LINQ02/Program.cs
+++ LINQ02/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            
+
+            #region Ordering Operators [Order by, Then By , Asc , Desc]
+
+            //Order By
+
+            //Select * from Products Order by Name asc, Price Desc
+
+
+            // Fluent Syntax
+
+            //var result = ProductList.OrderBy(p => p.UnitsInStock).ThenByDescending(p => p.UnitPrice);
+
+            //Query Syntax
+
+            //var result = from product in ProductList
+            //             orderby product.UnitsInStock, product.UnitPrice descending
+            //             select product;
+
+
+            #endregion
+
+
         }
     }
 }
