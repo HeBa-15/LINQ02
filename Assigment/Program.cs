a530444356cf9eb7aab24cac9cb24490f1235bb2
HeBa-15 <hebamahmoudali1510@gmail.com>
2025-02-01 01:22:38 +0200

    LINQ – Transformation Operators

--- Assigment/Program.cs
+++ Assigment/Program.cs
@@ -389,15 +389,15 @@ namespace Assigment
             #endregion
 
             #region Q 8. Create a list of all digits in the array whose second letter is 'i' that is reversed from the order in the original array.
-           
-            
+
+
             //string[] Arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
             //var result = Arr
             //            .Where(word => word.Length > 1 && word[1] == 'i')
             //            .Reverse()
             //            .ToList();
-           
+
             //foreach (var item in result)
             //{
             //    Console.WriteLine(item);
@@ -405,6 +405,116 @@ namespace Assigment
             //}
 
             #endregion
+
+
+
+            #endregion
+
+            #region LINQ – Transformation Operators
+
+            #region  Q 1. Return a sequence of just the names of a list of products
+
+            //var result = ProductList.Select(p => p.ProductName);
+
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion
+
+            #region Q 2. Produce a sequence of the uppercase and lowercase versions of each word in the original array (Anonymous Types).
+
+
+            //String[] words = { "aPPLE", "BlUeBeRrY", "cHeRry" };
+
+            //var result = words
+            //                .Select(word => new { Upper = word.ToUpper(), Lower = word.ToLower() })
+            //                .ToList();
+
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion
+
+            #region Q 3. Produce a sequence containing some properties of Products, including UnitPrice which is renamed to Price in the resulting type.
+
+            //var result = ProductList
+            //                       .Select(p => new { p.ProductName, Price = p.UnitPrice, p.UnitsInStock })
+            //                       .ToList();
+
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
+            #region Q 4. Determine if the value of int in an array matches their position in the array.
+
+            // int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            //var result = Arr
+            //           .Select((value, index) => new { Value = value, Index = index })
+            //           .ToList();
+
+            //Console.WriteLine("Number : In-Place ?");
+            //foreach (var item in result)
+            //{
+
+            //    Console.WriteLine($" {item.Value} : {item.Value == item.Index}");
+            //}
+
+            #endregion
+
+            #region Q 5. Returns all pairs of numbers from both arrays such that the number from numbersA is less than the number from numbersB.
+
+            //int[] numbersA = { 0, 2, 4, 5, 6, 8, 9 };
+            //int[] numbersB = { 1, 3, 5, 7, 8 };
+
+            //var result = from a in numbersA
+            //            from b in numbersB
+            //            where a < b
+            //            select new { A = a, B = b };
+
+            //Console.WriteLine("Pairs where a < b:");
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine($" {item.A} is less than {item.B}");
+            //}
+
+            #endregion
+
+            #region Q 6. Select all orders where the order total is less than 500.00
+
+            //var result = CustomerList
+            //           .SelectMany(c => c.Orders)
+            //           .Where(o => o.Total < 500.00m)
+            //           .ToList();
+
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            #endregion
+
+            #region Q 7. Select all orders where the order was made in 1998 or later
+
+            //var result = CustomerList
+            //           .SelectMany(c => c.Orders)
+            //           .Where(o => o.OrderDate.Year >= 1998)
+            //           .ToList();
+
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion
+
 
 
 
